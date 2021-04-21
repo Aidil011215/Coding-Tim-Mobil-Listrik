a0000d290caef407f92d07ba106e5e9c2f426234
Rifqi falih <79573404+Rifqi-Falih@users.noreply.github.com>
2021-04-21 14:24:35 +0700

    Update pedal_elektric_code.ino

--- STM32 BLUEPILL/pedal_elektric_code/pedal_elektric_code.ino
+++ STM32 BLUEPILL/pedal_elektric_code/pedal_elektric_code.ino
@@ -2,12 +2,12 @@ int pedal = PA1;
 int state = 0;
 
 void setup() {
-  pinMode(pedal, OUTPUT); //set pedal as output
+  pinMode(pedal, OUTPUT); //set pedal as an output
   Serial.begin(9600);
   // initialize serial communication at 9600 bits per second
 }
 
-// the loop routine runs over and over again forever:
+// the loop routine runs over and over again forever
 void loop() {
   // read the input on analog pin 0
    state = (analogRead(pedal)-770)/3.26;
